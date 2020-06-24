b3bd3c1ced1ab1865ecde9c4cce1b7a3b3e7c0b0
osorio <osorio-cruz@hotmail.com>
2020-06-24 18:01:04 -0400

    Add Repo file to project. Create API request call.

--- app/src/main/java/com/example/movieapp/repository/Repository.kt
+++ app/src/main/java/com/example/movieapp/repository/Repository.kt
@@ -1,9 +1,40 @@
 package com.example.movieapp.repository
 
+import android.util.Log
 import com.example.movieapp.BuildConfig
+import com.example.movieapp.Movie
+import com.example.movieapp.MovieApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Repository {
 
     private val API_KEY = BuildConfig.OMDB_API
 
+    fun getMovie() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://www.omdbapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val movieApi: MovieApi =  retrofit.create(MovieApi::class.java)
+
+        val call: Call<Movie> = movieApi.getMovieInformation(API_KEY, "Batman")
+
+        call.enqueue(object : Callback<Movie> {
+            override fun onFailure(call: Call<Movie>, t: Throwable) {
+                Log.d("TEST", "Error: ${t.message}")
+            }
+
+            override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
+                Log.d("TEST", "Success: ${response.body()}")
+                val movie = response.body()
+                Log.d("TEST", "${movie!!.title}")
+            }
+
+        })
+    }
 }
