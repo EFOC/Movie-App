503db4c510716e05ec83e46d176d94afc985aa59
osorio <osorio-cruz@hotmail.com>
2020-06-26 05:57:15 -0400

    Create new fragment to search movies.

---
+++ app/src/main/java/com/example/movieapp/ui/main/SearchMovieFragment.kt
@@ -0,0 +1,32 @@
+package com.example.movieapp.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+class SearchMovieFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchMovieFragment()
+    }
+
+    private lateinit var fragmentViewModel: SearchMovieFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.search_movie_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        fragmentViewModel = ViewModelProviders.of(this).get(SearchMovieFragmentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
