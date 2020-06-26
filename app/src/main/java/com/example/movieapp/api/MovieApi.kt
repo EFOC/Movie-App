99950ab5d87e18381c1a45b180599914e083ed79
osorio <osorio-cruz@hotmail.com>
2020-06-26 03:10:14 -0400

    Change parameter to 's' to get a list of movies.

--- app/src/main/java/com/example/movieapp/api/MovieApi.kt
+++ app/src/main/java/com/example/movieapp/api/MovieApi.kt
@@ -1,13 +1,13 @@
 package com.example.movieapp.api
 
 import com.example.movieapp.model.Movie
+import com.example.movieapp.model.MovieList
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieApi {
 
-//    http://www.omdbapi.com/?apikey=66c04664&t=*broke*
     @GET("https://www.omdbapi.com/")
-fun getMovieInformation(@Query("apikey") apiKey: String, @Query("t") movieName: String): Call<Movie>
+fun getMovieInformation(@Query("apikey") apiKey: String, @Query("s") movieName: String): Call<MovieList>
 }
