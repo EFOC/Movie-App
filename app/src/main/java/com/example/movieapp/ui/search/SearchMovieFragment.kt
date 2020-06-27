6ba6ba4cb20e489fac91f9d712e5454583297307
osorio <osorio-cruz@hotmail.com>
2020-06-26 21:14:05 -0400

    Connect recyclerview to fragment. Use Livedata to send and display list to recyclerview.

--- app/src/main/java/com/example/movieapp/ui/search/SearchMovieFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/search/SearchMovieFragment.kt
@@ -7,9 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.R
+import com.example.movieapp.adapter.MovieListAdapter
 import com.example.movieapp.databinding.SearchMovieFragmentBinding
+import com.example.movieapp.repository.Repository
 
 class SearchMovieFragment : Fragment() {
 
@@ -17,11 +22,12 @@ class SearchMovieFragment : Fragment() {
         fun newInstance() = SearchMovieFragment()
     }
 
+    private val repo = Repository()
     private lateinit var searchMovieFragmentViewModel: SearchMovieFragmentViewModel
     private lateinit var binding: SearchMovieFragmentBinding
+    private lateinit var movieRecyclerView: RecyclerView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.search_movie_fragment, container, false)
         return binding.root
     }
@@ -32,4 +38,12 @@ class SearchMovieFragment : Fragment() {
         binding.viewmodel = searchMovieFragmentViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        movieRecyclerView = view.findViewById(R.id.search_movie_fragment_recycler_view)
+        repo.getMovie().observe(viewLifecycleOwner, Observer {movieList ->
+            movieRecyclerView.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+            movieRecyclerView.adapter = MovieListAdapter(movieList)
+        })
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
