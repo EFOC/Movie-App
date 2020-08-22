d6bb6ebffcd6218f2e6af1d6c3eb9f7839505a6f
osorio <osorio-cruz@hotmail.com>
2020-08-22 01:24:58 -0400

    Add description to part of the Movie class.

--- app/src/main/java/com/example/movieapp/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/model/Movie.kt
@@ -4,6 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Movie(
     @SerializedName("Title") val title: String,
+    @SerializedName("Plot") val plot: String,
     @SerializedName("Year") val year: String,
     @SerializedName("imdbID") val id: String,
     @SerializedName("Poster") val posterImage: String)
