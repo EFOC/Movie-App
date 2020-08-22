52169401435f223b065b6c42d9a89cd9edce46fc
osorio <osorio-cruz@hotmail.com>
2020-08-22 01:24:33 -0400

    Create view and viewModel for the movie details.

---
+++ app/src/main/java/com/example/movieapp/ui/search/MovieDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.example.movieapp.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.movieapp.R
+
+class MovieDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MovieDetailFragment()
+    }
+
+    private lateinit var viewModel: MovieDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.movie_detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MovieDetailViewModel::class.java)
+    }
+
+}
