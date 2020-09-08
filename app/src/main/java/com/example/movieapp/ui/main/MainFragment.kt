7934d55ff29beca0ca4a7f591c9e10961d40f11a
osorio <osorio-cruz@hotmail.com>
2020-09-08 00:11:47 -0400

    Insert nav controller to main fragment.

--- app/src/main/java/com/example/movieapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/main/MainFragment.kt
@@ -1,12 +1,14 @@
 package com.example.movieapp.ui.main
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.movieapp.R
 import com.example.movieapp.databinding.MainFragmentBinding
 
@@ -18,11 +20,18 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+//        navController.navigate(R.id.action_mainFragment_to_searchMovieFragment)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
