a5b1ffca2dedd10fc59e9c8f16641c3f4c2ca527
osorio <osorio-cruz@hotmail.com>
2020-09-08 00:07:26 -0400

    Add Toolbar to main activity Link toolbar menu items to nav controller

--- app/src/main/java/com/example/movieapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/activity/MainActivity.kt
@@ -5,21 +5,30 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.movieapp.R
-import com.example.movieapp.ui.main.MainFragment
-import com.example.movieapp.ui.search.SearchMovieFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        toolbar = findViewById(R.id.mainToolBar)
+        setSupportActionBar(toolbar)
+        appBarConfiguration = AppBarConfiguration(navController.graph, null)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -28,17 +37,12 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.main_menu_search_item -> changeFragment(SearchMovieFragment.newInstance())
-            R.id.main_menu_home_item -> changeFragment(MainFragment.newInstance())
-        }
-        return super.onOptionsItemSelected(item)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    private fun changeFragment(newFragment: Fragment) {
-        if (newFragment.isVisible)
-            return
-        supportFragmentManager.beginTransaction().replace(R.id.container, newFragment).commit()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
