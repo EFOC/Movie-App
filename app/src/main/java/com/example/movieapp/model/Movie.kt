a46c3656cdfaef3c5fe355eb1b7e92eb68f5949d
osorio <osorio-cruz@hotmail.com>
2020-09-11 05:01:33 -0400

    Make Movie class parcelable to send over fragments.

--- app/src/main/java/com/example/movieapp/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/model/Movie.kt
@@ -1,10 +1,13 @@
 package com.example.movieapp.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Movie(
     @SerializedName("Title") val title: String,
     @SerializedName("Plot") val plot: String,
     @SerializedName("Year") val year: String,
     @SerializedName("imdbID") val id: String,
-    @SerializedName("Poster") val posterImage: String)
+    @SerializedName("Poster") val posterImage: String) : Parcelable
