17a5da2176b682e55d57f9c132d60ba9fa9d48de
osorio <osorio-cruz@hotmail.com>
2020-09-28 22:06:47 -0400

    Make fragment observe one list.

--- app/src/main/java/com/example/movieapp/ui/search/SearchMovieFragmentViewModel.kt
+++ app/src/main/java/com/example/movieapp/ui/search/SearchMovieFragmentViewModel.kt
@@ -1,24 +1,38 @@
 package com.example.movieapp.ui.search
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.movieapp.model.Movie
 import com.example.movieapp.repository.Repository
 
 class SearchMovieFragmentViewModel : ViewModel() {
 
     val editTextContent = MutableLiveData<String>()
-    var movieList = Repository.getTrendingMovies()
+    val finalList = MediatorLiveData<List<Movie>>()
 
 
-    fun buttonClick() {
-        this.movieList = getMovieList(editTextContent.value.toString())
+    fun setSelection(selection: Int) {
+        when(selection){
+            1 -> addTrendingList()
+            2 -> addSearchList()
+        }
+    }
+
+    private fun addTrendingList() {
+        finalList.removeSource(Repository.getTrendingMovies())
+        finalList.addSource(Repository.getTrendingMovies()) {
+            finalList.value = it
+        }
+    }
+
+    private fun addSearchList() {
+        finalList.removeSource(Repository.getMovieList(editTextContent.value.toString()))
+        finalList.addSource(Repository.getMovieList(editTextContent.value.toString())) {
+            finalList.value = it
+        }
     }
 
     fun getMovieDetail(movieId: String): MutableLiveData<Movie> {
         return Repository.getMovieDetail(movieId)
     }
-    private fun getMovieList(movieSearch: String): MutableLiveData<List<Movie>> = Repository.getMovieList(movieSearch)
 
 }
