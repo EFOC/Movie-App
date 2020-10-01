b03c3fa4e347df560663916bcc01a3fd0a0e8086
osorio <osorio-cruz@hotmail.com>
2020-10-01 15:58:42 -0400

    Seperate the API call to different file.

--- app/src/main/java/com/example/movieapp/repository/Repository.kt
+++ app/src/main/java/com/example/movieapp/repository/Repository.kt
@@ -7,6 +7,7 @@ import com.example.movieapp.BuildConfig
 import com.example.movieapp.model.Movie
 import com.example.movieapp.api.MovieApi
 import com.example.movieapp.model.MovieList
+import com.example.movieapp.util.ApiServiceGenerator
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,46 +17,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 object Repository {
 
     private const val API_KEY = BuildConfig.THEMOVIEDB;
-    private lateinit var liveDataMovies: MutableLiveData<Any>
-    private var movieApi: MovieApi
+    private val apiServiceGenerator = ApiServiceGenerator()
+    private var movieApi: MovieApi = ApiServiceGenerator.createService()
 
-    init {
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-         movieApi =  retrofit.create(
-            MovieApi::class.java)
-    }
-
-    private fun <T> serviceGen(call: Call<T>, isList: Boolean): MutableLiveData<Any> {
-        liveDataMovies = MutableLiveData()
-        call.enqueue(object : Callback<T>{
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                Log.d("TEST", "Error: ${t.message}")
-            }
-
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                val movie = response.body()
-                if (isList)
-                    liveDataMovies.value = (movie as MovieList).movies
-                else
-                    liveDataMovies.value = movie
-            }
-        })
-        return liveDataMovies
-    }
 
     fun getMovieDetail(movieId: String): MutableLiveData<Movie> =
-        serviceGen(movieApi.getMovieDetail(movieId, API_KEY), false) as MutableLiveData<Movie>
+        apiServiceGenerator.movieaApiCall(movieApi.getMovieDetail(movieId, API_KEY), false) as MutableLiveData<Movie>
 
     fun getMovieList(movieSearch: String): MutableLiveData<List<Movie>> =
-        serviceGen(movieApi.getMovieInformation(API_KEY, movieSearch), true) as MutableLiveData<List<Movie>>
+        apiServiceGenerator.movieaApiCall(movieApi.getMovieInformation(API_KEY, movieSearch), true) as MutableLiveData<List<Movie>>
 
     fun getTrendingMovies(): MutableLiveData<List<Movie>> =
-        serviceGen(movieApi.getTrendingMovies(API_KEY), true) as MutableLiveData<List<Movie>>
+        apiServiceGenerator.movieaApiCall(movieApi.getTrendingMovies(API_KEY), true) as MutableLiveData<List<Movie>>
 
     fun getPopularMovies(): MutableLiveData<List<Movie>> =
-        serviceGen(movieApi.getPopularMovies(API_KEY), true) as MutableLiveData<List<Movie>>
+        apiServiceGenerator.movieaApiCall(movieApi.getPopularMovies(API_KEY), true) as MutableLiveData<List<Movie>>
 }
--- app/src/main/java/com/example/movieapp/util/ApiServiceGenerator.kt
+++ app/src/main/java/com/example/movieapp/util/ApiServiceGenerator.kt
@@ -1,28 +1,48 @@
 package com.example.movieapp.util
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.example.movieapp.api.MovieApi
+import com.example.movieapp.model.MovieList
+import com.example.movieapp.repository.Repository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiServiceGenerator {
 
-    private lateinit var movieApi: MovieApi
+    companion object {
+        fun createService(): MovieApi {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
+            return retrofit.create(
+                MovieApi::class.java)
+        }
+    }
     init {
         createService()
     }
 
-    fun getService(): MovieApi {
-        return movieApi
-    }
+    fun <T> movieaApiCall(call: Call<T>, isList: Boolean) : MutableLiveData<Any> {
+        val liveDataMovies: MutableLiveData<Any> = MutableLiveData()
+        call.enqueue(object : Callback<T> {
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                Log.d("TEST", "Error: ${t.message}")
+            }
 
-    private fun createService() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://www.omdbapi.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val movieApi: MovieApi =  retrofit.create(
-            MovieApi::class.java)
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                val movie = response.body()
+                if (isList)
+                    liveDataMovies.value = (movie as MovieList).movies
+                else
+                    liveDataMovies.value = movie
+            }
+        })
+        return liveDataMovies
     }
 }
