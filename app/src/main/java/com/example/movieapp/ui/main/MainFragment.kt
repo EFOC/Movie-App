e3c955e7b2240d6eaede01012365a11664a9d1aa
osorio <osorio-cruz@hotmail.com>
2020-10-02 16:45:15 -0400

    Rename viewmodel file.

--- app/src/main/java/com/example/movieapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.ui.main
 
+import android.accounts.AccountManager
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -9,6 +10,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.movieapp.R
 import com.example.movieapp.databinding.MainFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainFragment : Fragment() {
 
@@ -16,12 +18,13 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var fragmentViewModel: MainFragmentViewModel
     private lateinit var binding: MainFragmentBinding
     private lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -33,8 +36,8 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        binding.viewModel = viewModel
+        fragmentViewModel = ViewModelProvider(this).get(MainFragmentViewModel::class.java)
+        binding.viewModel = fragmentViewModel
         // TODO: Use the ViewModel
     }
 
