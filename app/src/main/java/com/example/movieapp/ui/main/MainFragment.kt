92128df6a5fb0a980f9f1c449876b59787b43f13
osorio <osorio-cruz@hotmail.com>
2020-10-06 19:11:59 -0400

    Create sign in flow for users to sign in.

--- app/src/main/java/com/example/movieapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/main/MainFragment.kt
@@ -1,7 +1,8 @@
 package com.example.movieapp.ui.main
 
-import android.accounts.AccountManager
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -10,6 +11,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.movieapp.R
 import com.example.movieapp.databinding.MainFragmentBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainFragment : Fragment() {
@@ -24,7 +27,7 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
-
+        binding.loginButton.setOnClickListener { launchSignInFlow() }
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -41,4 +44,24 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response = IdpResponse.fromResultIntent(data)
+        if (requestCode == 1001) {
+            Log.d("TEST", "Success sign in by ${FirebaseAuth.getInstance().currentUser?.displayName}")
+            navController.navigate(R.id.searchMovieFragment)
+        } else {
+            Log.d("TEST", "failed sign in by ${response?.error?.errorCode}")
+        }
+    }
+
+    private fun launchSignInFlow() {
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build(), AuthUI.IdpConfig.PhoneBuilder().build())
+
+        startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build(),
+        1001)
+    }
 }
