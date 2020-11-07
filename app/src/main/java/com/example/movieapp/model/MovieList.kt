a5df8eb45a2b2a0a2d298b1025f23ce94321d369
osorio <osorio-cruz@hotmail.com>
2020-11-06 21:30:23 -0500

    Parcelize class and clean it up

--- app/src/main/java/com/example/movieapp/model/MovieList.kt
+++ app/src/main/java/com/example/movieapp/model/MovieList.kt
@@ -1,9 +1,8 @@
 package com.example.movieapp.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
-class MovieList {
-
-    @SerializedName("results")
-    lateinit var movies: List<Movie>
-}
+@Parcelize
+class MovieList(@SerializedName("results") val movies: List<Movie>): Parcelable
