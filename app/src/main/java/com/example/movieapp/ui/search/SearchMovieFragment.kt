2a327d7a3440834911d17b027204a7f4b6f4b91e
osorio <osorio-cruz@hotmail.com>
2020-11-07 00:09:36 -0500

    Add swipe function to list items

--- app/src/main/java/com/example/movieapp/ui/search/SearchMovieFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/search/SearchMovieFragment.kt
@@ -2,26 +2,24 @@ package com.example.movieapp.ui.search
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.R
 import com.example.movieapp.adapter.MovieListAdapter
 import com.example.movieapp.databinding.SearchMovieFragmentBinding
-import com.example.movieapp.model.Movie
-import com.example.movieapp.model.MovieList
 import com.example.movieapp.ui.search.SearchMovieFragmentViewModel.Selection.*
 import com.firebase.ui.auth.AuthUI
+import java.util.*
 
 class SearchMovieFragment : Fragment(), MovieSearchItemViewModel {
 
@@ -32,6 +30,7 @@ class SearchMovieFragment : Fragment(), MovieSearchItemViewModel {
     private lateinit var searchMovieFragmentViewModel: SearchMovieFragmentViewModel
     private lateinit var binding: SearchMovieFragmentBinding
     private lateinit var movieRecyclerView: RecyclerView
+    private lateinit var adapter: MovieListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -53,14 +52,30 @@ class SearchMovieFragment : Fragment(), MovieSearchItemViewModel {
         movieRecyclerView = binding.searchMovieFragmentRecyclerView.apply {
             this.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         }
-        val adapter = MovieListAdapter(searchMovieFragmentViewModel)
+        adapter = MovieListAdapter(searchMovieFragmentViewModel)
         adapter.setCallback(this)
+        setItemTouch()
         binding.searchMovieFragmentRecyclerView.adapter = adapter
         searchMovieFragmentViewModel.setSelection(TRENDINGLIST)
         searchMovieFragmentViewModel.finalList.observe(viewLifecycleOwner, Observer {movieList ->
             adapter.submitList(movieList)
             binding.searchMovieFragmentRecyclerView.scrollToPosition(0)
         })
+    }
+
+    private fun setItemTouch() {
+        ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                searchMovieFragmentViewModel.removeMovieFromDatabase(adapter.getItemAt(viewHolder.adapterPosition).id)
+                //TODO: Fix the UserList section
+                searchMovieFragmentViewModel.setSelection(USERLIST)
+            }
+        }).attachToRecyclerView(movieRecyclerView)
     }
 
     override fun displayMovieDetailsButton(movieId: String) {
