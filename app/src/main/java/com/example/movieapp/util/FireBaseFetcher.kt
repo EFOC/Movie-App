1fc9ee2dc7447f95cf0c35815863e9c74098c26f
osorio <osorio-cruz@hotmail.com>
2020-11-09 19:50:56 -0500

    Remove unused imports

--- app/src/main/java/com/example/movieapp/util/FireBaseFetcher.kt
+++ app/src/main/java/com/example/movieapp/util/FireBaseFetcher.kt
@@ -1,7 +1,5 @@
 package com.example.movieapp.util
 
-import android.app.Application
-import android.content.res.Resources
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
